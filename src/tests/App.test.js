18aab7f68d74cda9ec3c3f6e728cbc92e7e33061
abhivikrant <vikrantabhishek72@gmail.com>
2020-09-25 17:51:22 +0530

    Did Unit testing and it failed for one.

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "../components/App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/abhishek/i);
   expect(linkElement).toBeInTheDocument();
+});
+test("renders abhishek ", () => {
+  const { getAllByText } = render(<App />);
+  const linkElement = getAllByText(/abhishek/i);
+  expect(linkElement.length).toBe(6);
 });
